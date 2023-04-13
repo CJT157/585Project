8884efbf5476b6bde5b86feaad7f765dbb84ac85
Colin Truemner <cjaytruemner@gmail.com>
2023-04-13 11:46:53 -0400

    basic table setup code

---
+++ bigquery/create_dataset.py
@@ -0,0 +1,19 @@
+from google.cloud import bigquery
+
+# Connect to Bigquery
+client = bigquery.Client(project="cps585finalproject")
+
+# TODO(developer): Set dataset_id to the ID of the dataset to create.
+dataset_id = "{}.stock_data".format(client.project)
+
+# Construct a full Dataset object to send to the API.
+dataset = bigquery.Dataset(dataset_id)
+
+# TODO(developer): Specify the geographic location where the dataset should reside.
+dataset.location = "US"
+
+# Send the dataset to the API for creation, with an explicit timeout.
+# Raises google.api_core.exceptions.Conflict if the Dataset already
+# exists within the project.
+dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
+print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
---
+++ bigquery/setup_bigquery.sh
@@ -0,0 +1,4 @@
+# these definitely do not work yet
+# python3 ./create_dataset.sh
+
+# python3 ./create_table.sh
