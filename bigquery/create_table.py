3aeb1e82c3e7e25a4c5eb6ee7a580733d5bd7fe5
Colin Truemner <cjaytruemner@gmail.com>
2023-04-17 17:48:45 -0400

    remade tables

--- bigquery/create_table.py
+++ bigquery/create_table.py
@@ -3,9 +3,10 @@ from google.cloud import bigquery
 # Construct a BigQuery client object.
 client = bigquery.Client()
 
-table_id = "cps585finalproject.stock_data.company_data"
+table_id_company = "cps585finalproject.stock_data.company_data"
+table_id_analyst = "cps585finalproject.stock_data.analyst_data"
 
-schema = [
+schema_company = [
     bigquery.SchemaField("symbol", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("time", "DATETIME", mode="REQUIRED"),
     bigquery.SchemaField("open_price", "FLOAT", mode="REQUIRED"),
@@ -13,6 +14,12 @@ schema = [
     bigquery.SchemaField("low_price", "FLOAT", mode="REQUIRED"),
     bigquery.SchemaField("close_price", "FLOAT", mode="REQUIRED"),
     bigquery.SchemaField("volume", "INTEGER", mode="REQUIRED"),
+]
+
+schema_analyst = [
+    bigquery.SchemaField("symbol", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("date", "DATE", mode="REQUIRED"),
+    bigquery.SchemaField("time", "TIME", mode="REQUIRED"),
     bigquery.SchemaField("action_pt", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("action_company", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("rating_current", "STRING", mode="REQUIRED"),
@@ -20,8 +27,14 @@ schema = [
     bigquery.SchemaField("adjusted_pt_current", "FLOAT", mode="REQUIRED"),
 ]
 
-table = bigquery.Table(table_id, schema=schema)
-table = client.create_table(table)  # Make an API request.
+table_company = bigquery.Table(table_id_company, schema=schema_company)
+table_company = client.create_table(table_company)  # Make an API request.
+
+table_analyst = bigquery.Table(table_id_analyst, schema=schema_analyst)
+table_analyst = client.create_table(table_analyst)  # Make an API request.
 print(
-    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+    "Created table {}.{}.{}".format(table_company.project, table_company.dataset_id, table_company.table_id)
+)
+print(
+    "Created table {}.{}.{}".format(table_analyst.project, table_analyst.dataset_id, table_analyst.table_id)
 )
--- cloud_functions/get_stock_data.py
+++ cloud_functions/get_stock_data.py
@@ -2,6 +2,7 @@ import functions_framework
 from alpaca_trade_api import REST
 from benzinga import financial_data
 from dotenv import load_dotenv
+from google.cloud import bigquery
 import os
 
 """
@@ -28,12 +29,39 @@ def get_stock_data(request):
   companies = ["MSFT","AAPL","DIS","JNJ"]
 
   # Request data for these companies from the trade API
-  bars = api.get_bars(companies)
+  bars = api.get_snapshots(companies)
   ratings = fin.ratings(company_tickers="MSFT,AAPL,DIS,JNJ")
 
+  # Print out data for each company
   for rating in ratings['ratings']:
     print(f"{rating['ticker']} : {rating['action_pt']} | {rating['action_company']} | {rating['rating_current']} | {rating['pt_current']} | {rating['adjusted_pt_current']}")
 
+
+  # get bigquery client/table
+  client = bigquery.Client()
+
+  table_id_company = bigquery.Table.from_string("cps585finalproject.stock_data.company_data")
+  table_id_analyst = bigquery.Table.from_string("cps585finalproject.stock_data.analyst_data")
+
+
+  # Format data in list for bigquery
+
+  rows_to_insert_company = [{"symbol": symbol, "time": bars[symbol].daily_bar.t, "open_price": bars[symbol].daily_bar.o, 
+                             "high_price": bars[symbol].daily_bar.h, "low_price": bars[symbol].daily_bar.l, 
+                             "close_price": bars[symbol].daily_bar.c, "volume": bars[symbol].daily_bar.v} for symbol in bars]
+
+  rows_to_insert_analyst = [{"symbol": rating['ticker'], "time": rating['time'], "date": rating['date'], "action_pt": rating['action_pt'], 
+                             "action_company": rating['action_company'], "rating_current": rating['rating_current'], "pt_current": rating['pt_current'], 
+                            "adjusted_pt_current": rating['adjusted_pt_current']} for rating in ratings['ratings']]
+  
+
+  # # Insert data into bigquery
+
+  # errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
+  # if errors == []:
+  #     print("New rows have been added.")
+  # else:
+  #     print("Encountered errors while inserting rows: {}".format(errors))
 
   # Return an HTTP response
   return 'OK'
